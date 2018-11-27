8ba5087bd451ff6f28146ebd43ce75773ca7a29e
Michael Kruger <michaelkrugerus@gmail.com>
2018-11-27 09:32:24 -0800

    Fix typo and remove trailing whitespace

--- trunk/SUAVE/Components/Energy/Networks/Solar.py
+++ trunk/SUAVE/Components/Energy/Networks/Solar.py
@@ -1,6 +1,6 @@
 ## @ingroup Components-Energy-Networks
 # Solar.py
-# 
+#
 # Created:  Jun 2014, E. Botero
 # Modified: Feb 2016, T. MacDonald
 
@@ -24,33 +24,33 @@ from SUAVE.Core import Data
 ## @ingroup Components-Energy-Networks
 class Solar(Propulsor):
     """ A solar powered system with batteries and maximum power point tracking.
-        
+
         This network adds an extra unknowns to the mission, the torque matching between motor and propeller.
-    
+
         Assumptions:
         None
-        
+
         Source:
         None
-    """      
+    """
     def __defaults__(self):
         """ This sets the default values for the network to function.
-    
+
             Assumptions:
             None
-    
+
             Source:
             N/A
-    
+
             Inputs:
             None
-    
+
             Outputs:
             None
-    
+
             Properties Used:
             N/A
-        """            
+        """
         self.solar_flux        = None
         self.solar_panel       = None
         self.motor             = None
@@ -64,36 +64,36 @@ class Solar(Propulsor):
         self.engine_length     = None
         self.number_of_engines = None
         self.tag               = 'network'
-    
+
     # manage process with a driver function
     def evaluate_thrust(self,state):
         """ Calculate thrust given the current state of the vehicle
-    
+
             Assumptions:
             Caps the throttle at 110% and linearly interpolates thrust off that
-    
+
             Source:
             N/A
-    
+
             Inputs:
             state [state()]
-    
+
             Outputs:
             results.thrust_force_vector [newtons]
             results.vehicle_mass_rate   [kg/s]
             conditions.propulsion:
-                solar_flux           [watts/m^2] 
+                solar_flux           [watts/m^2]
                 rpm                  [radians/sec]
                 current              [amps]
                 battery_draw         [watts]
                 battery_energy       [joules]
                 motor_torque         [N-M]
                 propeller_torque     [N-M]
-    
+
             Properties Used:
             Defaulted values
-        """          
-    
+        """
+
         # unpack
         conditions  = state.conditions
         numerics    = state.numerics
@@ -106,10 +106,10 @@ class Solar(Propulsor):
         payload     = self.payload
         solar_logic = self.solar_logic
         battery     = self.battery
-       
+
         # Set battery energy
         battery.current_energy = conditions.propulsion.battery_energy
-        
+
         # step 1
         solar_flux.solar_radiation(conditions)
         # link
@@ -125,61 +125,61 @@ class Solar(Propulsor):
         # Step 4
         esc.voltageout(conditions)
         # link
-        motor.inputs.voltage = esc.outputs.voltageout 
+        motor.inputs.voltage = esc.outputs.voltageout
         # step 5
         motor.omega(conditions)
         # link
         propeller.inputs.omega =  motor.outputs.omega
         # step 6
         F, Q, P, Cplast = propeller.spin(conditions)
-     
+
         # Check to see if magic thrust is needed, the ESC caps throttle at 1.1 already
         eta = conditions.propulsion.throttle[:,0,None]
         P[eta>1.0] = P[eta>1.0]*eta[eta>1.0]
         F[eta>1.0] = F[eta>1.0]*eta[eta>1.0]
-        
+
         # Run the avionics
         avionics.power()
         # link
         solar_logic.inputs.pavionics =  avionics.outputs.power
-        
+
         # Run the payload
         payload.power()
         # link
         solar_logic.inputs.ppayload = payload.outputs.power
-        
+
         # Run the motor for current
         motor.current(conditions)
         # link
         esc.inputs.currentout =  motor.outputs.current
-        
+
         # Run the esc
         esc.currentin()
         # link
         solar_logic.inputs.currentesc  = esc.outputs.currentin*self.number_of_engines
-        solar_logic.inputs.volts_motor = esc.outputs.voltageout 
+        solar_logic.inputs.volts_motor = esc.outputs.voltageout
         #
         solar_logic.logic(conditions,numerics)
         # link
         battery.inputs = solar_logic.outputs
         battery.energy_calc(numerics)
-        
+
         #Pack the conditions for outputs
         rpm                                  = motor.outputs.omega*60./(2.*np.pi)
         current                              = solar_logic.inputs.currentesc
-        battery_draw                         = battery.inputs.power_in 
+        battery_draw                         = battery.inputs.power_in
         battery_energy                       = battery.current_energy
-        
-        conditions.propulsion.solar_flux       = solar_flux.outputs.flux  
+
+        conditions.propulsion.solar_flux       = solar_flux.outputs.flux
         conditions.propulsion.rpm              = rpm
         conditions.propulsion.current          = current
         conditions.propulsion.battery_draw     = battery_draw
         conditions.propulsion.battery_energy   = battery_energy
         conditions.propulsion.motor_torque     = motor.outputs.torque
-        conditions.propulsion.propeller_torque = Q        
-        
+        conditions.propulsion.propeller_torque = Q
+
         #Create the outputs
-        F    = self.number_of_engines * F * [1,0,0]      
+        F    = self.number_of_engines * F * [1,0,0]
         mdot = np.zeros_like(F)
 
         results = Data()
@@ -187,62 +187,62 @@ class Solar(Propulsor):
         results.vehicle_mass_rate   = mdot
 
         return results
-    
-    
+
+
     def unpack_unknowns(self,segment,state):
-        """ This is an extra set of unknowns which are unpacked from the mission solver and send to the network.
-    
+        """ This is an extra set of unknowns which are unpacked from the mission solver and sent to the network.
+
             Assumptions:
             None
-    
+
             Source:
             N/A
-    
+
             Inputs:
             state.unknowns.propeller_power_coefficient [None]
-    
+
             Outputs:
             state.conditions.propulsion.propeller_power_coefficient [None]
-    
+
             Properties Used:
             N/A
-        """       
-        
+        """
+
         # Here we are going to unpack the unknowns (Cp) provided for this network
         state.conditions.propulsion.propeller_power_coefficient = state.unknowns.propeller_power_coefficient
 
         return
-    
+
     def residuals(self,segment,state):
         """ This packs the residuals to be send to the mission solver.
-    
+
             Assumptions:
             None
-    
+
             Source:
             N/A
-    
+
             Inputs:
             state.conditions.propulsion:
                 motor_torque                          [N-m]
                 propeller_torque                      [N-m]
-            
+
             Outputs:
             None
-    
+
             Properties Used:
             None
-        """  
-        
+        """
+
         # Here we are going to pack the residuals from the network
-        
+
         # Unpack
         q_motor   = state.conditions.propulsion.motor_torque
         q_prop    = state.conditions.propulsion.propeller_torque
-        
+
         # Return the residuals
         state.residuals.network[:,0] = q_motor[:,0] - q_prop[:,0]
-        
-        return        
-            
+
+        return
+
     __call__ = evaluate_thrust
