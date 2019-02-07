6a880a5e56d05364fe5b7b20dba4c5c80c011c18
Michael Kruger <michaelkrugerus@gmail.com>
2019-02-07 11:41:30 -0800

    yapf file

--- trunk/SUAVE/Components/Energy/Networks/Unified_Network.py
+++ trunk/SUAVE/Components/Energy/Networks/Unified_Network.py
@@ -8,9 +8,6 @@
 #  Imports
 # ----------------------------------------------------------------------
 
-# suave imports
-import SUAVE
-
 # package imports
 import numpy as np
 from SUAVE.Core import Data
@@ -21,74 +18,76 @@ from SUAVE.Components.Propulsors.Propulsor import Propulsor
 #  Network
 # ----------------------------------------------------------------------
 
+
 ## @ingroup Components-Energy-Networks
 class Unified_Network(Propulsor):
     """ Simply connects a battery to a ducted fan, with an assumed motor efficiency
-    
+
         Assumptions:
         None
-        
+
         Source:
         None
     """
-    
+
     def __defaults__(self):
         """ This sets the default values for the network to function.
             This network operates slightly different than most as it attaches a propulsor to the net.
-    
+
             Assumptions:
             Your system always uses 90 amps...?
-    
+
             Source:
             N/A
-    
+
             Inputs:
             None
-    
+
             Outputs:
             None
-    
+
             Properties Used:
             N/A
-        """         
-        
-        self.propulsor        = None
-        self.battery          = None
-        self.motor_efficiency = .95 
-        self.tag              = 'Network'
-    
+        """
+
+        self.propulsor = None
+        self.battery = None
+        self.motor_efficiency = .95
+        self.tag = 'Network'
+
     # manage process with a driver function
-    def evaluate_thrust(self,state):
+    def evaluate_thrust(self, state):
         """ Calculate thrust given the current state of the vehicle
-    
+
             Assumptions:
             None
-    
+
             Source:
             N/A
-    
+
             Inputs:
             state [state()]
-    
+
             Outputs:
             results.thrust_force_vector [newtons]
             results.vehicle_mass_rate   [kg/s]
-    
+
             Properties Used:
             Defaulted values
-        """         
-        
+        """
+
         # unpack
 
-        propulsor   = self.propulsor
-        battery     = self.battery
-    
-        conditions  = state.conditions
-        numerics    = state.numerics
-  
+        propulsor = self.propulsor
+        battery = self.battery
+
+        conditions = state.conditions
+        numerics = state.numerics
+
         results = propulsor.evaluate_thrust(state)
-        Pe      = np.multiply(results.thrust_force_vector[:,0],conditions.freestream.velocity[0])
-        
+        Pe = np.multiply(results.thrust_force_vector[:, 0],
+                         conditions.freestream.velocity[0])
+
         # try:
         #     initial_energy = conditions.propulsion.battery_energy
         #     if initial_energy[0][0]==0: #beginning of segment; initialize battery
@@ -98,31 +97,31 @@ class Unified_Network(Propulsor):
 
         # Set battery energy
         battery.current_energy = conditions.propulsion.battery_energy
-        
-        pbat = -Pe/self.motor_efficiency
-        battery_logic          = Data()
+
+        pbat = -Pe / self.motor_efficiency
+        battery_logic = Data()
         battery_logic.power_in = pbat
-        battery_logic.current  = 90.  #use 90 amps as a default for now; will change this for higher fidelity methods
-      
+        battery_logic.current = 90.  #use 90 amps as a default for now; will change this for higher fidelity methods
+
         battery.inputs = battery_logic
-        tol = 1e-6
-        
+
         battery.energy_calc(numerics)
         #allow for mass gaining batteries
-       
+
         try:
-            mdot=find_mass_gain_rate(battery,-(pbat-battery.resistive_losses)) #put in transpose for solver
+            mdot = find_mass_gain_rate(battery, -(
+                pbat - battery.resistive_losses))  #put in transpose for solver
         except AttributeError:
-            mdot=np.zeros_like(results.thrust_force_vector[:,0])
-        mdot=np.reshape(mdot, np.shape(conditions.freestream.velocity))
+            mdot = np.zeros_like(results.thrust_force_vector[:, 0])
+        mdot = np.reshape(mdot, np.shape(conditions.freestream.velocity))
         #Pack the conditions for outputs
-        battery_draw                         = battery.inputs.power_in
-        battery_energy                       = battery.current_energy
-      
-        conditions.propulsion.battery_draw   = battery_draw
+        battery_draw = battery.inputs.power_in
+        battery_energy = battery.current_energy
+
+        conditions.propulsion.battery_draw = battery_draw
         conditions.propulsion.battery_energy = battery_energy
-        
-        results.vehicle_mass_rate   = mdot
+
+        results.vehicle_mass_rate = mdot
         return results
-            
+
     __call__ = evaluate_thrust
