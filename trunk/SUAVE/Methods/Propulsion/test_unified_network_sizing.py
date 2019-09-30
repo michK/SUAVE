6deb2d361a09525c718e6b1e78fef2ce901f4d35
Michael Kruger <michaelkrugerus@gmail.com>
2019-09-30 16:42:22 -0700

    Fixed propsys sizing unit test

--- trunk/SUAVE/Methods/Propulsion/test_unified_network_sizing.py
+++ trunk/SUAVE/Methods/Propulsion/test_unified_network_sizing.py
@@ -22,6 +22,7 @@ class TestUnifiedNetworkSizing(unittest.TestCase):
         # Define propsys input variables
         propsys.number_of_engines_mech = 2
         propsys.number_of_engines_elec = 4
+        propsys.fBLIm = 0.0
 
         vehicle.append_component(propsys)
 
@@ -29,6 +30,10 @@ class TestUnifiedNetworkSizing(unittest.TestCase):
         vehicle.mdottot_cruise = 500 * Units['kg/s']
         vehicle.fL_cruise = 0.5
         vehicle.cruise_mach = 0.23943836
+        vehicle.cruise_mach = 0.6
+
+        vehicle.nr_engines_mech = propsys.number_of_engines_mech
+        vehicle.nr_engines_elec = propsys.number_of_engines_elec
 
         wing = SUAVE.Components.Wings.Main_Wing()
         wing.tag = 'main_wing'
@@ -42,14 +47,21 @@ class TestUnifiedNetworkSizing(unittest.TestCase):
         fuselage.effective_diameter = 0.5 * (fuselage.width + fuselage.heights.maximum)
         vehicle.append_component(fuselage)
 
-        propsys = unified_network_sizing(vehicle.propulsors.unified_propsys, vehicle)
+        propsys = unified_network_sizing(propsys, vehicle)
 
-        self.assertAlmostEqual(propsys.mech_fan_dia, 0.712, places=0, msg="Should be about 0.712")
-        self.assertAlmostEqual(propsys.elec_fan_dia, 0.712, places=0, msg="Should be about 0.712")
-        self.assertAlmostEqual(propsys.mech_nac_dia, 0.879, places=0, msg="Should be about 0.879")
-        self.assertAlmostEqual(propsys.elec_nac_dia, 0.879, places=0, msg="Should be about 0.879")
-        self.assertAlmostEqual(propsys.areas_wetted_mech, 6.64, places=0, msg="Should be about 6.64")
-        self.assertAlmostEqual(propsys.areas_wetted_elec, 1.66, places=0, msg="Should be about 1.66")
+        print("propsys.mech_fan_dia: {}".format(propsys.mech_fan_dia))
+        print("propsys.elec_fan_dia: {}".format(propsys.elec_fan_dia))
+        print("propsys.mech_nac_dia: {}".format(propsys.mech_nac_dia))
+        print("propsys.elec_nac_dia: {}".format(propsys.elec_nac_dia))
+        print("propsys.areas_wetted_mech: {}".format(propsys.areas_wetted_mech))
+        print("propsys.areas_wetted_elec: {}".format(propsys.areas_wetted_elec))
+
+        self.assertAlmostEqual(propsys.mech_fan_dia, 0.986, places=3, msg="Should be about 0.986")
+        self.assertAlmostEqual(propsys.elec_fan_dia, 0.697, places=3, msg="Should be about 0.697")
+        self.assertAlmostEqual(propsys.mech_nac_dia, 1.233, places=3, msg="Should be about 1.232")
+        self.assertAlmostEqual(propsys.elec_nac_dia, 0.872, places=3, msg="Should be about 0.872")
+        self.assertAlmostEqual(propsys.areas_wetted_mech, 7.879, places=3, msg="Should be about 7.878")
+        self.assertAlmostEqual(propsys.areas_wetted_elec, 1.970, places=3, msg="Should be about 1.96")
 
 if __name__ == '__main__':
     unittest.main()
