90d3025ded53ae0fc02eb912d819d993cbec89c6
Michael Kruger <michaelkrugerus@gmail.com>
2019-11-04 09:26:15 -0800

    Fix fan sizing

--- trunk/SUAVE/Methods/Propulsion/unified_network_sizing.py
+++ trunk/SUAVE/Methods/Propulsion/unified_network_sizing.py
@@ -88,6 +88,13 @@ def unified_network_sizing(propsys, vehicle, f_KED_wing=0.5):
     # Electrical fan diameters
     propsys.elec_fan_dia = Dfane = np.sqrt(4 * Afane / np.pi)
 
+    # Set diameters to zero in the case of configurations where they 'disappear'
+    if fL <= 0.01:  # Propulsion only from mechanical side
+        propsys.elec_fan_dia = Dfane = 0
+    elif fL >= 0.99:  # Propulsion only from electrical side
+        propsys.mech_fan_dia = Dfanm = 0
+
+
     #########################
     # Mechanical propulsors #
     #########################
