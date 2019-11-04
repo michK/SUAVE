d78d3f1275b4166f288c2694d826ab9a64f16ef7
Michael Kruger <michaelkrugerus@gmail.com>
2019-11-04 09:23:43 -0800

    Bugfix

--- trunk/SUAVE/Input_Output/Results/identify_architecture.py
+++ trunk/SUAVE/Input_Output/Results/identify_architecture.py
@@ -8,21 +8,21 @@ def identify_arch(propsys):
     fS = propsys.info.fS_max
     fL = propsys.info.fL_max
 
-    if (fS <= 0.05 and fL <= 0.05):
+    if (fS <= 0.01 and fL <= 0.01):
         arch = 'Conventional'
-    elif (fS <= 0.05 and 0 <= fL <= 1):
+    elif (fS <= 0.01 and 0.01 <= fL <= 0.99):
         arch = 'Partial Turbo-electric'
-    elif (fS >= 0.95 and 0 <= fL <= 1):
+    elif (fS <= 0.01 and 0.99 <= fL):
         arch = 'Full Turbo-electric'
-    elif (0 <= fS <= 1 and fL <= 0.05):
+    elif (0 <= fS <= 1 and fL <= 0.01):
         arch = 'Parallel Hybrid'
     elif (0 <= fS <= 1 and 0 <= fL <= 1):
         arch = 'Series/Parallel Partial Hybrid'
-    elif (0 <= fS <= 1 and fL >= 0.95):
+    elif (0 <= fS <= 1 and fL >= 0.99):
         arch = 'Series Hybrid'
-    elif (fS >= 0.95 and fL >= 0.95):
+    elif (fS >= 0.99 and fL >= 0.99):
         arch = 'All-electric'
-    elif (fS >= 0.95 and fL <= 0.05) or (fS >= 0.95 and 0 <= fL <= 1):
+    elif (fS >= 0.99 and fL <= 0.01) or (fS >= 0.99 and 0 <= fL <= 1):
         arch = 'Check design: Configuration not physical'
 
     return arch
