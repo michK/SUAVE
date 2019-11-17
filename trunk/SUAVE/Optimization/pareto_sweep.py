b8658543bfe4021e640866af47d6b6b2ebdcd3f3
Michael Kruger <michaelkrugerus@gmail.com>
2019-11-17 11:25:02 -0800

    Implement Pareto sweep
    - This function is adapted from Optimization/line_plot.py
      and re-runs the optimization problem for every point in
      the sweep, leading to a fully sized and optimized
      aircraft at every point
    - The function plots the optimization objective versus the
      user specified sweep variable
    - The function was tested using PyOptSparse with both the SLSQP
      and SNOPT solvers

--- trunk/SUAVE/Optimization/pareto_sweep.py
+++ trunk/SUAVE/Optimization/pareto_sweep.py
@@ -1,7 +1,7 @@
 ## @ingroup Optimization
 #  pareto_sweep.py
 #
-# Created  : Jul 2018, M. Kruger
+# Created  : Nov 2019, M. Kruger
 # Modified :
 
 # ----------------------------------------------------------------------
@@ -11,19 +11,19 @@
 import SUAVE
 from SUAVE.Core import Data
 from .Package_Setups import pyoptsparse_setup
-from SUAVE.Input_Output.Results import print_SNOPT_summary
+from .Package_Setups import pyopt_setup
+from .Package_Setups import scipy_setup
+
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-import os
 
 # ----------------------------------------------------------------------
 #  pareto_sweep
 # ----------------------------------------------------------------------
 
 
-def pareto_sweep(problem, print_PSEC, number_of_points, sweep_index, datafile='data.out'):
+def pareto_sweep(problem, number_of_points, sweep_index):
     """
     Takes in an optimization problem and runs a Pareto sweep of the sweep index sweep_index.
     i.e. sweep_index=0 means you want to sweep the first variable, sweep_index = 4 is the 5th variable)
@@ -31,6 +31,8 @@ def pareto_sweep(problem, print_PSEC, number_of_points, sweep_index, datafile='d
     with the added functionality that it runs the optimization problem for every point in the sweep,
     not just evaluate the objective function with other design variables fixed at their initial values,
     such as in line_plot()
+
+    Users can update line 85 and specify their optimizer of choice
 
         Assumptions:
         N/A
@@ -61,51 +63,43 @@ def pareto_sweep(problem, print_PSEC, number_of_points, sweep_index, datafile='d
     bnd              = base_inputs[:,2] # Bounds
     scl              = base_inputs[:,3] # Scaling
     base_objective   = opt_prob.objective
-    obj_name         = base_objective[0][0] #objective function name (used for scaling)
+    obj_name         = base_objective[0][0] # Objective function name (used for scaling)
     obj_scaling      = base_objective[0][1]
     base_constraints = opt_prob.constraints
 
-    #define inputs, output, and constraints for sweep
+    # Define inputs, output, and constraints for sweep
     inputs = np.zeros([2,number_of_points])
-    obj    = np.zeros([number_of_points])
-    PSEC   = np.zeros([number_of_points])
+    obj = np.zeros([number_of_points])
+    constraint_num  = np.shape(base_constraints)[0] # of constraints
+    constraint_val  = np.zeros([constraint_num, number_of_points])
 
     #create inputs matrix
     inputs[0,:] = np.linspace(bnd[idx0][0], bnd[idx0][1], number_of_points)
 
     # Create file to write results into
-    data_path = os.path.join(os.path.expanduser('.'), 'Data',datafile)
-    with open(data_path, "w+") as f:
-        f.write("Data file for sweep of PSEC vs fL\n")
-        #inputs defined; now run sweep
-        for i in range(0, number_of_points):
-            opt_prob.inputs[:,1][idx0]= inputs[0,i]
+    for i in range(0, number_of_points):
+        opt_prob.inputs[:,1][idx0]= inputs[0,i]
 
-            opt_prob.inputs[idx0][2] = (inputs[0,i], inputs[0,i])
-            problem.optimization_problem = opt_prob
-            sol = pyoptsparse_setup.Pyoptsparse_Solve(problem, solver='SNOPT', FD='parallel', sense_step=1e-06)
-            obj[i] = sol.fStar * obj_scaling
-            PSEC[i] = problem.summary.PSEC
-
-            if print_SNOPT_summary('/home/michael/Dropbox/PhD/Research/Codes/CADA/CADA/Commuter/SNOPT_summary.out'):
-                converged = 1
-            else:
-                converged = 0
-
-            f.write("{}, {}, {}\n".format(inputs[0,i], PSEC[i], converged))
+        opt_prob.inputs[idx0][2] = (inputs[0,i], inputs[0,i])
+        problem.optimization_problem = opt_prob
+        sol = pyoptsparse_setup.Pyoptsparse_Solve(
+            problem, solver='SLSQP', FD='parallel', sense_step=1e-06)
+        obj[i] = problem.objective() * obj_scaling
+        constraint_val[:,i] = problem.all_constraints().tolist()
 
     # Create plot
-    # fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-    # if print_PSEC:
-    #     ax.plot(inputs[0,:], PSEC, lw = 2)
-    #     ax.set_xlabel(names[idx0])
-    #     ax.set_ylabel('PSEC [kJ/kg/km]')
-    # else:
-    #     ax.plot(inputs[0,:], obj, lw = 2)
-    #     ax.set_xlabel(names[idx0])
-    #     ax.set_ylabel(obj_name)
+    ax.plot(inputs[0,:], obj, lw = 2)
+    ax.set_xlabel(names[idx0])
+    ax.set_ylabel(obj_name)
 
-    # plt.show()
+    plt.show(block=True)
 
-    return
+    # Pack outputs
+    outputs = Data()
+    outputs.inputs = inputs
+    outputs.objective = obj
+    outputs.constraint_val = constraint_val
+
+    return outputs
